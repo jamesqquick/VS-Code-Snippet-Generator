de3ea7617bac12a7bbd680984a64b1d69a23dd55
James Quick <james.q.quick@outlook.com>
2018-05-07 08:06:08 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,52 @@
+const formattedInput = document.getElementById('formattedInput');
+const snippetBody = document.getElementById('snippetBody');
+const snippetName = document.getElementById('snippetName');
+const snippetPrefix = document.getElementById('snippetPrefix');
+const snippetDescription = document.getElementById('snippetDescription');
+const copyBtn = document.getElementById('copyBtn');
+
+console.log('hello');
+
+snippetName.value = 'name';
+snippetPrefix.value = 'prefix';
+snippetDescription.value = 'Here is my super cool description';
+snippetBody.value = `<html>
+	<body>
+	</body>
+</html>`;
+
+let snippet = {};
+const updateSnippetOutput = () => {
+	formattedInput.value = `"${snippetName.value}": {
+	"prefix":"${snippetPrefix.value}",
+	"body": [
+		${formatSnippetBody(snippetBody.value)}
+	],
+	"description":"${snippetDescription.value}"
+}`;
+};
+
+const formatSnippetBody = body => {
+	const bodyArray = body.split('\n');
+	return bodyArray
+		.map((line, index) => {
+			const formattedLine = line.replace(/"/g, '\\"');
+			return ` ${index !== 0 ? '\t\t' : ''}"${formattedLine}"${
+				index !== bodyArray.length - 1 ? ',\n' : ''
+			}`;
+		})
+		.join('');
+};
+
+snippetName.addEventListener('keyup', updateSnippetOutput);
+snippetPrefix.addEventListener('keyup', updateSnippetOutput);
+snippetDescription.addEventListener('keyup', updateSnippetOutput);
+
+snippetBody.addEventListener('keyup', updateSnippetOutput);
+
+copyBtn.addEventListener('click', e => {
+	formattedInput.select();
+	document.execCommand('copy');
+});
+
+updateSnippetOutput();
