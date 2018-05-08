32d916c579886ab50976c65233181a2ed49af091
James Quick <james.q.quick@outlook.com>
2018-05-08 15:54:08 -0500

    updated port variable for correct syntax

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
 	console.log(`listening on port ${port}`);
 });
